41035b5bd7d4f77c31b3643f4ecdca3fd3044f36
George Savva <36664190+georgemsavva@users.noreply.github.com>
2020-08-09 09:29:36 +0100

    update lines

--- lines/spiro983.R
+++ lines/spiro983.R
@@ -3,11 +3,13 @@ z <- 1i*sin(t*pi)+1*cos(t*pi)+exp(983i*pi*t)*(t*6)%%1
 x=Re(z)
 y=Im(z)
 l=max(Mod(z))*1.2
+h=(t/2)%%.5
+s=sin(t*10)^2/2
 png("temp.png", width=2000, height=2000, type="cairo-png", antialias = "subpixel")
 par(mar=c(0,0,0,0), bg="#373737")
 plot(NA, axes=FALSE, xlim=c(-l,l), ylim=c(-l,l))
 segments(x,y,c(x[-1], NA), 
          c(y[-1], NA), 
          lwd=ifelse(2*((t%%(2/20))>=1/20),2,-1),
-         col=rgb(1,1,1,.7))
+         col=hsv(h,s,1,.9))
 dev.off()
